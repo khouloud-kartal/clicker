1f6a7856bf74134419a0f77be913e8e0b6f3b077
khouloud-kartal <khouloud.kartal@laplateforme.io>
2023-03-01 13:47:34 +0100

    the game works, but the interval is not registered when the page is reloaded

--- clicker.js
+++ clicker.js
@@ -1,24 +1,45 @@
 const image = document.getElementById('image');
 let clickNum = document.getElementById('clickNum');
-let title = document.getElementById('title');
+let title1 = document.getElementById('title1');
+let title2 = document.getElementById('title2');
+let title3 = document.getElementById('title3');
 
-const store = document.querySelector('.store');
+const store1 = document.querySelector('.store1');
+const store2 = document.querySelector('.store2');
+const store3 = document.querySelector('.store3');
 
 // clickNum.innerText = 0
 
 
 //add a click in our local storage each time we click on our flower
-image.addEventListener("click", (e)=>{
-    clickNum.innerText++; 
-    localStorage.setItem('clickNum', clickNum.innerText);
-})
+
+//if our storage is null give it the title in our html value
+if(localStorage.getItem('clickNum') === null){
+
+    localStorage.setItem('clickNum', clickNum.innerText)
+
+    image.addEventListener("click", (e)=>{
+        clickNum.innerText++; 
+        localStorage.setItem('clickNum', clickNum.innerText);
+    })
+}else{
+
+
+    image.addEventListener("click", (e)=>{
+        clickNum.innerText++; 
+        localStorage.setItem('clickNum', clickNum.innerText);
+    })
+
+}
+
+
 
 clickNum.innerText = localStorage.getItem('clickNum')
 
 
 // let store1Int = parseInt(localStorage.getItem('white flower'));
-let store3Int = parseInt(localStorage.getItem('pink flower'));
-let store4Int = parseInt(localStorage.getItem('red flower'));
+// let store3Int = parseInt(localStorage.getItem('pink flower'));
+// let store4Int = parseInt(localStorage.getItem('red flower'));
 
 let storeInt = parseInt(localStorage.getItem('clickNum'));
 
@@ -30,14 +51,14 @@ let storeInt = parseInt(localStorage.getItem('clickNum'));
 
 
 const store1Btn = document.getElementById('store1Btn')
-store.appendChild(store1Btn)
+store1.appendChild(store1Btn)
 
 let store1Int = parseInt(localStorage.getItem('white flower'));
 
 
 if(localStorage.getItem('white flower') === null){
 
-    localStorage.setItem('white flower', title.innerText)
+    localStorage.setItem('white flower', title1.innerText)
 
    
 
@@ -46,7 +67,7 @@ if(localStorage.getItem('white flower') === null){
     
 }else{
 
-    title.innerText = localStorage.getItem('white flower')
+    title1.innerText = localStorage.getItem('white flower')
 
     store1Btn.addEventListener("click", changePrice1)  
 
@@ -55,13 +76,15 @@ if(localStorage.getItem('white flower') === null){
 //function to change the price
 function changePrice1(){
 
-    title.innerText = localStorage.getItem('white flower')
+    title1.innerText = localStorage.getItem('white flower')
     let store1Int = parseInt(localStorage.getItem('white flower'));
+    
 
     console.log(store1Int)
 
     if(clickNum.innerText >= store1Int){
-
+        
+        setInterval(myTimer, 10000);
         
         clickNum.innerText = clickNum.innerText - store1Int
         localStorage.setItem('clickNum', clickNum.innerText);
@@ -71,7 +94,7 @@ function changePrice1(){
         store1Int = Math.round(store1Int)
         
         localStorage.setItem('white flower', store1Int);
-        title.innerText = localStorage.getItem('white flower')
+        title1.innerText = localStorage.getItem('white flower')
         
     }
     else {
@@ -84,47 +107,122 @@ function changePrice1(){
 
 //////////////////////////////////////////////store2//////////////////////////////////
 
-// //create and display the store number one
-// const store2 = document.createElement('h3')
-// store.appendChild(store2)
+const store2Btn = document.getElementById('store2Btn')
+store2.appendChild(store2Btn)
 
-// const store2Btn = document.createElement('button')
-// store.appendChild(store2Btn)
-// store2Btn.setAttribute("name", "store2")
-// store2Btn.setAttribute("class", "store2")
-// store2Btn.setAttribute("id", "store2")
-
-// store2.innerText = 15
-// store2Btn.innerText = 'yellow flower'
-// store2.innerText = localStorage.getItem('yellow flower')
-
-// store2Btn.addEventListener("click", changePrice2)
+let store2Int = parseInt(localStorage.getItem('pink flower'));
 
 
-// //function to change the price
-// function changePrice2(){
-//     localStorage.setItem('yellow flower', '15');
+if(localStorage.getItem('pink flower') === null){
 
+    localStorage.setItem('pink flower', title2.innerText)
 
-//     if(clickNum.innerText >= store1Int){
-//         clickNum.innerText = clickNum.innerText - store1Int
-//         localStorage.setItem('clickNum', clickNum.innerText);
-        
-//         store2Int *= 1.2
+   
 
-//         store2Int = Math.round(store2Int)
+    store2Btn.addEventListener("click", changePrice2)  
 
-//         localStorage.setItem('yellow flower', store2Int);
-//         store2.innerText = localStorage.getItem('yellow flower')
-
-//     }else{
-//         alert('You do not have enough click')
-//     }
     
-// }
+}else{
 
-// function myTimer() {
-//     clickNum.innerText++; 
-//     localStorage.setItem('clickNum', clickNum.innerText);
-// }
-// setInterval(myTimer, 1000);
+    title2.innerText = localStorage.getItem('pink flower')
+
+    store2Btn.addEventListener("click", changePrice2)  
+
+}
+
+//function to change the price
+function changePrice2(){
+
+    title2.innerText = localStorage.getItem('pink flower')
+    let store2Int = parseInt(localStorage.getItem('pink flower'));
+    
+
+    // console.log(store1Int)
+
+    if(clickNum.innerText >= store2Int){
+
+        setInterval(myTimer, 5000);
+        
+        clickNum.innerText = clickNum.innerText - store2Int
+        localStorage.setItem('clickNum', clickNum.innerText);
+        
+        store2Int *= 1.6
+        
+        store2Int = Math.round(store2Int)
+        
+        localStorage.setItem('pink flower', store2Int);
+        title2.innerText = localStorage.getItem('pink flower')
+        
+    }
+    else {
+        alert("you do not have enough clicks")
+    }
+    
+}
+
+
+
+
+//////////////////////////////////////////////store3//////////////////////////////////
+
+const store3Btn = document.getElementById('store3Btn')
+store3.appendChild(store3Btn)
+
+let store3Int = parseInt(localStorage.getItem('red flower'));
+
+
+if(localStorage.getItem('red flower') === null){
+
+    localStorage.setItem('red flower', title3.innerText)
+
+    store3Btn.addEventListener("click", changePrice3)  
+
+    
+}else{
+
+    title3.innerText = localStorage.getItem('red flower')
+
+    store3Btn.addEventListener("click", changePrice3)  
+
+}
+
+//function to change the price
+function changePrice3(){
+
+    title3.innerText = localStorage.getItem('red flower')
+    let store3Int = parseInt(localStorage.getItem('red flower'));
+
+    // console.log(store1Int)
+
+    if(clickNum.innerText >= store3Int){
+
+        setInterval(myTimer, 1000);
+        
+        clickNum.innerText = clickNum.innerText - store3Int
+        localStorage.setItem('clickNum', clickNum.innerText);
+        
+        store3Int *= 1.6
+        
+        store3Int = Math.round(store3Int)
+        
+        localStorage.setItem('red flower', store3Int);
+        title3.innerText = localStorage.getItem('red flower')
+        
+    }
+    else {
+        alert("you do not have enough clicks")
+    }
+    
+}
+
+
+
+
+
+
+
+
+function myTimer() {
+    clickNum.innerText++; 
+    localStorage.setItem('clickNum', clickNum.innerText);
+}
---
+++ clicker.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="clicker.js" defer></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="click">
+        <img src="./flower.png" alt="white flower" id="image">
+        <h2>Click</h2>
+        <h3 id="clickNum">0</h3>
+    </div>
+    <div class="store">
+        <div class="store1">
+            <h3 id="title1">1</h3>
+            <button id="store1Btn" >white flower</button>
+        </div>
+
+        <div class="store2">
+            <h3 id="title2">5</h3>
+            <button id="store2Btn" >Pink flower</button>
+        </div>
+
+        <div class="store3">
+            <h3 id="title3">10</h3>
+            <button id="store3Btn" >Red flower</button>
+        </div>
+    </div>
+</body>
+</html>
